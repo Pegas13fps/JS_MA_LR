9fa7045ab82e2e53510cb941e0e220d186eeca62
Pegas13fps <Tymoschenkob@gmail.com>
2019-02-16 22:11:44 +0200

    added example

---
+++ learnJS/modules/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+;(function() { // ; - safe from dickhead =)
+  let message = 'Hello';
+
+  function showMessage() {
+    console.log(message);
+  }
+
+  showMessage();
+}());
+
+// Function Expression
