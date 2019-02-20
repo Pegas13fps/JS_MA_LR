6e439fa69fbc68621df37ed713fdb42cc18f0326
Pegas13fps <Tymoschenkob@gmail.com>
2019-02-20 18:42:33 +0200

    added search element 3 ways

---
+++ learnJS/searchElement/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let li = ul.getElementsByTagName('li')[1]; // live list
+
+let li = ul.querySelector('li:nth-child(2)'); // statick list
+
+let li = ul.querySelectorAll('li')[1]; // long way for search
