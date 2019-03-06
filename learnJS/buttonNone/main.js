fb725ad38dbaf5da4d7fb20bc2f9ff4d21963ce5
Pegas13fps <Tymoschenkob@gmail.com>
2019-03-06 17:17:23 +0200

    added another task

--- learnJS/buttonNone/main.js
+++ learnJS/buttonNone/main.js
@@ -3,5 +3,9 @@
 let btn = document.querySelector('.hider');
 
 btn.onclick = function() {
-    document.querySelector('.text').style.display = 'none';
+    document.querySelector('.text').style.display = 'none'; // text none
   };
+
+// btn.onclick = function() {
+//     this.style.display = 'none'; // button none
+//   };
